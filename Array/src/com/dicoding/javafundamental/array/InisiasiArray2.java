17795d00eef37051012d408a8161640660c0085a
Nurus Sobah <73220374+nurusken12@users.noreply.github.com>
2020-12-17 07:53:49 +0700

    Add files via upload

---
+++ Array/src/com/dicoding/javafundamental/array/InisiasiArray2.java
@@ -0,0 +1,20 @@
+package com.dicoding.javafundamental.array;
+
+public class InisiasiArray2 {
+    public static void main(String[] args) {
+        int[] arrInt = new int[6];
+        arrInt[0] = 1;
+        arrInt[1] = 2;
+        arrInt[2] = 3;
+        arrInt[3] = 4;
+        arrInt[4] = 5;
+        arrInt[5] = 6;
+
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+        System.out.println(arrInt[5]);
+    }
+}
